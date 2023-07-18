ad75b2cb47986f16d7f400045dda1f18bc50bf48
ChengsiGod <139726762+ChengsiGod@users.noreply.github.com>
2023-07-18 15:48:03 -0600

    finish

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const about = document.querySelector("#about");
 const contact = document.querySelector("#contact");
+const project = document.querySelector("#projects");
 const aboutContent = document.querySelector("#about-content");
 const contactContent = document.querySelector("#contact-content");
+const projectContent = document.querySelector("#projects-content");
 const textElement = document.getElementById("effect");
+const textElement2 = document.getElementById("effect2");
 const text = "Chengsi Zhou";
+const text2 =
+  "I am a front end developer, I have a good understanding of HTML, CSS, JavaScript, and have basic knowledge of Git. I am also familiar with React...";
 let index = 0;
+let num = 0;
 
 // Create an object to track open windows
 let openWindows = {
@@ -12,14 +18,23 @@ let openWindows = {
   contact: null,
 };
 
-const writeText = () => {
+const writeText = (callback) => {
   if (index < text.length) {
     textElement.innerText = text.slice(0, index + 1);
     index++;
-    setTimeout(writeText, 700);
+    setTimeout(() => writeText(callback), 700); // Passing the callback
+  } else {
+    callback(); // Call the callback function after all text has been written
   }
 };
 
+const writeText2 = () => {
+  if (num < text2.length) {
+    textElement2.innerText = text2.slice(0, num + 1);
+    num++;
+    setTimeout(writeText2, 100);
+  }
+};
 about.addEventListener("click", () => {
   if (openWindows.about) return; // Check if the about window is already open, if yes, return
 
@@ -68,4 +83,28 @@ contact.addEventListener("click", () => {
   });
 });
 
-writeText();
+project.addEventListener("click", () => {
+  if (openWindows.about) return; // Check if the about window is already open, if yes, return
+
+  openWindows.about = new WinBox({
+    title: "projects",
+    width: "400px",
+    height: "400px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: projectContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+    onclose: function () {
+      openWindows.about = null; // Clear the about window from the object when it is closed
+    },
+  });
+});
+
+writeText(writeText2);
